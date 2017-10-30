fed48e50c5d6092485ca38092dcc0e3c932628d3
Anchiqua <anja.leskovsek@gmail.com>
2017-10-30 20:45:24 +0100

    popravila

--- dolzina.py
+++ dolzina.py
@@ -9,12 +9,14 @@ def dolzina_med_tockami(seznam_slovarjev):
 
     seznam = []                                                                    #seznam, kamor bomo shranili podsezname
     for i in seznam_slovarjev:                                                     #za vsako tocko bomo izracunali razdalje do njenih sosed
-        podseznam = []                                                             #podseznam, kjer bomo shranili razdalje za neko tocko
-        for k in seznam_slovarjev:                                                 #zanka, po kateri bomo izracunali dolzino 
-            dolzina = math.sqrt((i["y"] - k["y"])**2 + (i["x"] - k["x"])**2)       #izracunamo dolzino med glavno tocko in njeno sosedo
-            podseznam.append(dolzina)                                              #dodamo dolzino v podseznam
+        podseznam = []                                                              #podseznam, kjer bomo shranili razdalje za neko tocko
+        for k in seznam_slovarjev:                                                  #zanka, po kateri bomo izracunali dolzino 
+            if seznam_slovarjev.index(k) <= seznam_slovarjev.index(i):              #zanka, ki pogleda, če smo dolžino za določeni točki že izračunali
+                    podseznam.append(0)                                             #če smo dolžino že izračunali, doda vrednost 0
+            else:
+                dolzina = math.sqrt((i["y"] - k["y"])**2 + (i["x"] - k["x"])**2)       #izracunamo dolzino med glavno tocko in njeno sosedo
+                podseznam.append(dolzina)                                              #dodamo dolzino v podseznam
 
         seznam.append(podseznam)                                                   #v seznam damo podsseznam za neko tocko
     return seznam
-        
     
