1757ab90788e8633ee98f2e0506448d29ea7271b
Anchiqua <anja.leskovsek@gmail.com>
2017-10-30 21:33:34 +0100

    preslikala matriko

--- dolzina.py
+++ dolzina.py
@@ -6,7 +6,7 @@ import math
 def dolzina_med_tockami(seznam_slovarjev):
 
 #Funkcija sprejme seznam slovarjev iz katerega generira seznam seznamov. Vsak podseznam predstavlja razdalje od določene točke do vseh ostalih točk.
-
+#Da nebi razdalje dvojno računali, bomo izračunali razdaljo med točkama le enkrat tako, da dobimo zgornje trikotno matriko, ki ji bomo na koncu prišteli vrednost iste transponirane matrike
     seznam = []                                                                    #seznam, kamor bomo shranili podsezname
     for i in seznam_slovarjev:                                                     #za vsako tocko bomo izracunali razdalje do njenih sosed
         podseznam = []                                                              #podseznam, kjer bomo shranili razdalje za neko tocko
@@ -17,6 +17,11 @@ def dolzina_med_tockami(seznam_slovarjev):
                 dolzina = math.sqrt((i["y"] - k["y"])**2 + (i["x"] - k["x"])**2)       #izracunamo dolzino med glavno tocko in njeno sosedo
                 podseznam.append(dolzina)                                              #dodamo dolzino v podseznam
 
-        seznam.append(podseznam)                                                   #v seznam damo podsseznam za neko tocko
-    return seznam
+        seznam.append(podseznam)                                                    #v seznam damo podsseznam za neko tocko. 
+    matrika=numpy.array(seznam)                                                     #seznam spremenimo v matriko. Dobimo zgornjetrikotno matriko 
+
+    matrika = matrika + matrika.transpose()                                         #matriki prištejemo njeno transponirano vrednost
+    return matrika
     
+
+
