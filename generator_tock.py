20fc604b0bc8aac1281f31211c70ae87e7bdc642
Charisa <korat.sara7@gmail.com>
2017-11-24 11:58:55 +0100

    Časovne zahtevnosti

--- generator_tock.py
+++ generator_tock.py
@@ -11,16 +11,17 @@ import math
 # (klicanje: npr. generator_tock.krog(10,10))             #
 ###########################################################
 
+# Komentar: pri dolocanju casovne zahtevnosti je n = st_tock
 
 def krog(polmer, st_tock):
     '''Funkcija sprejme polmer in stevilo tock, ki jih nakljucno izbere znotraj kroga s srediscem v (0,0).
     Fukcija vrne tocke v seznamu slovarjev oblike {'x' : x, 'y' : y}.'''
     seznam_tock = [{'x' : 0, 'y' : 0}]                        # seznam, v katerega shranjujemo slovarje nakljucnih tock
     while len(seznam_tock) < st_tock:                         # iscemo nakljucne tocke, dokler v seznamu ni dovolj tock
-        x = numpy.random.uniform(-polmer, polmer)             # x in y koordinato izberemo nakljucno, enakomerno porazdeljeno na intervalu polmera
-        y = numpy.random.uniform(-polmer, polmer)             # s tem dobimo tocke znotraj kvadrata s stranico iste dolzine kot premer kroga
-        if math.sqrt(x ** 2 + y ** 2)  <= polmer:             # pogledamo, ce je tocka znotraj kroga
-            seznam_tock.append({'x' : x, 'y' : y})            # ce je, koordinati zapisemo v slovar in ju shranimo v seznam tock
+        x = numpy.random.uniform(-polmer, polmer)       #O(1) # x in y koordinato izberemo nakljucno, enakomerno porazdeljeno na intervalu polmera
+        y = numpy.random.uniform(-polmer, polmer)       #O(1) # s tem dobimo tocke znotraj kvadrata s stranico iste dolzine kot premer kroga
+        if math.sqrt(x ** 2 + y ** 2)  <= polmer:       #O(1) # pogledamo, ce je tocka znotraj kroga
+            seznam_tock.append({'x' : x, 'y' : y})      #O(1) # ce je, koordinati zapisemo v slovar in ju shranimo v seznam tock
     return seznam_tock                                        # vrnemo seznam slovarjev tock
 
 
@@ -28,10 +29,10 @@ def kvadrat(dolzina_stranice, st_tock):
     '''Funkcija sprejme dolzino stranice kvadrata in st. tock, ki jih nakljucno izbere znotraj kvadrata s srediscem v (0,0).
     Funkcija vrne tocke v seznamu slovarjev.'''
     seznam_tock = [{'x' : 0, 'y' : 0}]
-    for tocka in range(st_tock):                                                # iscemo nakljucne tocke v kvadratu, centriranega v koordinatnem izhodiscu,
-        x = numpy.random.uniform(-dolzina_stranice/2, dolzina_stranice/2)       # zato gledamo interval od polovice dolzine stranic v negatino in pozitivno
-        y = numpy.random.uniform(-dolzina_stranice/2, dolzina_stranice/2)
-        seznam_tock.append({'x' : x, 'y' : y})
+    for tocka in range(st_tock):                                           #O(n) # iscemo nakljucne tocke v kvadratu, centriranega v koordinatnem izhodiscu,
+        x = numpy.random.uniform(-dolzina_stranice/2, dolzina_stranice/2)  #O(1) # zato gledamo interval od polovice dolzine stranic v negatino in pozitivno
+        y = numpy.random.uniform(-dolzina_stranice/2, dolzina_stranice/2)  #O(1)
+        seznam_tock.append({'x' : x, 'y' : y})                             #O(1)
     return seznam_tock
 
 
@@ -40,10 +41,10 @@ def pravokotnik(dolzina_stranice_a, dolzina_stranice_b, st_tock):
     koordinatni sistem.
     Funkcija vrne tocke v seznamu slovarjev.'''
     seznam_tock = [{'x' : 0, 'y' : 0}]
-    for tocka in range(st_tock):                                                # iskanje tock v pravokotniku je zelo slicno iskanju znotraj kvadrata
-        x = numpy.random.uniform(-dolzina_stranice_a/2, dolzina_stranice_a/2)
-        y = numpy.random.uniform(-dolzina_stranice_b/2, dolzina_stranice_b/2)
-        seznam_tock.append({'x' : x, 'y' : y})
+    for tocka in range(st_tock):                                               #O(n)      # iskanje tock v pravokotniku je zelo slicno iskanju znotraj kvadrata
+        x = numpy.random.uniform(-dolzina_stranice_a/2, dolzina_stranice_a/2)  #O(1)
+        y = numpy.random.uniform(-dolzina_stranice_b/2, dolzina_stranice_b/2)  #O(1)
+        seznam_tock.append({'x' : x, 'y' : y})                                 #O(1)
     return seznam_tock
 
 
@@ -53,10 +54,10 @@ def elipsa(dolzina_ax, dolzina_by, st_tock):
     Funkcija vrne nakljucno zgenerirane tocke v seznamu slovarjev.'''
     seznam_tock = [{'x' : 0, 'y' : 0}]
     while len(seznam_tock) < st_tock:                               # Tocke iscemo najprej znotraj pravokotnika s stranicama dolzina_ax in dolzina_by
-        x = numpy.random.uniform(-dolzina_ax, dolzina_ax)
-        y = numpy.random.uniform(-dolzina_by, dolzina_by)
-        if (x ** 2) / (dolzina_ax ** 2) + (y ** 2) / (dolzina_by ** 2) <= 1:  # Ko dobimo nakljucno izbiro tock znotraj pravokotnika, preverimo, ce
-            seznam_tock.append({'x': x, 'y': y})                    # je tocka znotraj elipse, tako da tocki vstavimo v neenabo za obmocje elipse.
+        x = numpy.random.uniform(-dolzina_ax, dolzina_ax)           #O(1)
+        y = numpy.random.uniform(-dolzina_by, dolzina_by)           #O(1)
+        if (x ** 2) / (dolzina_ax ** 2) + (y ** 2) / (dolzina_by ** 2) <= 1:  #O(1)   # Ko dobimo nakljucno izbiro tock znotraj pravokotnika, preverimo, ce
+            seznam_tock.append({'x': x, 'y': y})              #O(1)      # je tocka znotraj elipse, tako da tocki vstavimo v neenabo za obmocje elipse.
     return seznam_tock
 
 
@@ -67,16 +68,16 @@ def trikotnik(dolzina_stranice, st_tock):
     a2 = [dolzina_stranice/2, 0]
     a3 = [0, dolzina_stranice]
     # Izracunamo ploscino danega trikotnika
-    A = 1 / 2 * (abs(a1[0] * a2[1] + a2[0] * a3[1] + a3[0] * a1[1] - a2[0] * a1[1] + a3[0] * a2[1] - a1[0] * a3[1]))
+    A = 1 / 2 * (abs(a1[0] * a2[1] + a2[0] * a3[1] + a3[0] * a1[1] - a2[0] * a1[1] + a3[0] * a2[1] - a1[0] * a3[1])) #O(1)
     seznam_tock = []
     while len(seznam_tock) < st_tock:
-        x = numpy.random.uniform(-dolzina_stranice/2, dolzina_stranice/2)
-        y = numpy.random.uniform(-dolzina_stranice/2, dolzina_stranice/2)
+        x = numpy.random.uniform(-dolzina_stranice/2, dolzina_stranice/2)                                   #O(1)
+        y = numpy.random.uniform(-dolzina_stranice/2, dolzina_stranice/2)                                   #O(1)
         # Izracunamo 3 ploscine, ki povezujejo novo nakljucno tocko z vsakim parom danih tock
         # Ce bo vsota treh ploscin enaka vsoti danega lika, tocka lezi znotraj trikotnika.
-        B = 1/2 * (abs(x * a2[1] + a2[0] * a3[1] + a3[0] * y - a2[0] * y + a3[0] * a2[1] - x * a3[1]))
-        C = 1/2 * (abs(a1[0] * y + x * a3[1] + a3[0] * a1[1] - x * a1[1] + a3[0] * y - a1[0] * a3[1]))
-        D = 1/2 * (abs(a1[0] * a2[1] + a2[0] * y + x * a1[1] - a2[0] * a1[1] + x * a2[1] - a1[0] * y))
-        if A == (B + C + D):                                # Ce tocka je znotraj trikotnika, jo damo v seznam tock.
-            seznam_tock.append({'x': x, 'y': y})
+        B = 1/2 * (abs(x * a2[1] + a2[0] * a3[1] + a3[0] * y - a2[0] * y + a3[0] * a2[1] - x * a3[1]))      #O(1)
+        C = 1/2 * (abs(a1[0] * y + x * a3[1] + a3[0] * a1[1] - x * a1[1] + a3[0] * y - a1[0] * a3[1]))      #O(1)
+        D = 1/2 * (abs(a1[0] * a2[1] + a2[0] * y + x * a1[1] - a2[0] * a1[1] + x * a2[1] - a1[0] * y))      #O(1)
+        if A == (B + C + D):                            #O(1)    # Ce tocka je znotraj trikotnika, jo damo v seznam tock.
+            seznam_tock.append({'x': x, 'y': y})        #O(1)
     return seznam_tock
--- minimalno_vpeto_drevo.py
+++ minimalno_vpeto_drevo.py
@@ -10,55 +10,57 @@ import math
 # matrika dolzin med tockami                              #
 ###########################################################
 
+# Komentar: pri dolocanju casovne zahtevnosti je n = stevilo tock
+
 def prim(seznam_tock, matrika_dolzin):
     '''Funkcija sprejme seznam slovarjev tock (return funkcij generatorjev tock) in matriko dolzin med
         temi tockami (return funkcije dolzina).
         Funkcija najde minimalno vpeto drevo. Izracunamo se vsoto cen minimalnega vpetega drevesa,
         ki pa ni del Primovega algoritma'''
-    st_vozlisc = len(seznam_tock)
+    st_vozlisc = len(seznam_tock)                                       #O(1)
     # Na zacetku pripravimo mnozico vozlisc, ki jih bomo za lepsi pregled oznacili z indeksi (0, 1, 2, ...)
     # Q bo vseboval vsa vozlisca, ki jih se nismo uporabili v seznamu F
-    Q = {i for i in range(st_vozlisc)}
+    Q = {i for i in range(st_vozlisc)}                                  #O(n)
     # Pripravimo seznam, za katerega bo veljalo: C[i] = najcenejsa povezava do i.
     # Sprva natavimo vse vrednosti na neskoncno.
-    C = [math.inf for i in range(st_vozlisc)]
+    C = [math.inf for i in range(st_vozlisc)]                           #O(n)
     # Nastavimo se vrednosti seznama E na "special flag value" (-1), kar kaze na to, da ne obstaja nobena povezava,
     # ki bi povezovala vozlisce E[i] s prejsnjimi vozlisci.
     # Komentar k izbiri "flag value" -> uporabimo -1 in ne npr. 0, ker imamo vozlisca nastavljena po indeksih: 0,1,...
-    E = [-1 for i in range(st_vozlisc)]
+    E = [-1 for i in range(st_vozlisc)]                                 #O(n)
     # Pripravimo prazen seznam F
     F = []
     korak = 0
     # Ponavljamo, dokler ne uporabimo vseh vozlisc (torej je mnozica Q prazna)
-    while len(Q):
+    while len(Q):    #O(n^2)
         # Najdemo najmanjso vrednost v C (iscemo po cenah vozlisc, ki so se v Q)
-        minimum = min([C[i] for i in Q])
+        minimum = min([C[i] for i in Q])                                 #O(n)
         # naredimo seznam vseh indeksov z minimumom (=minimum)
-        vsi_indeksi = [i for i, x in enumerate(C) if x == minimum]
+        vsi_indeksi = [i for i, x in enumerate(C) if x == minimum]       #O(n)
         # Odstranimo vozlisce iz mnozice neuporabljenih vozlisc (pazimo na morebitne ponovitve razdalj
         # ki so sicer pri nakljucni izbiri zelo malo verjetne)
         vozlisce = 0
-        for i in vsi_indeksi:
-            if i in Q:
-                vozlisce = i
-                Q.remove(i)
+        for i in vsi_indeksi:  #O(n)
+            if i in Q:         #O(1) - ker je mnozica
+                vozlisce = i   #O(1)
+                Q.remove(i)    #O(1) - ker je mnozica
                 break
         # Dodamo vozlisce v F
-        F.append(vozlisce)
+        F.append(vozlisce)     #O(1)
         # Ce E[vozlisce] nima vrednosti -1 ("flag value), dodamo E[vozlisce] v F
-        if E[vozlisce] != -1:
-            F.append(E[vozlisce])
+        if E[vozlisce] != -1:  #O(1)
+            F.append(E[vozlisce])   #O(1)
         # Zanka gre cez vsako vozlisce i, za katero velja, da med i in vozlisce obstaja povezava
-        for sosed in range(st_vozlisc):
-            cena = float(matrika_dolzin[vozlisce][sosed])
+        for sosed in range(st_vozlisc):     #O(n)
+            cena = float(matrika_dolzin[vozlisce][sosed])    #O(1)
             # Ce je vozlisce i v Q in je cena povezave med i in vozlisce manjsa od C[i],
             # potem C[i] nastavimo na zdajsnjo ceno povezave in v E[i] shranimo vozlisce, tako da kaze na
             # povezavo med i in vozlisce
-            if (sosed in Q) and (cena != 0) and (cena < C[sosed]):
-                C[sosed] = cena
-                E[sosed] = vozlisce
+            if (sosed in Q) and (cena != 0) and (cena < C[sosed]):   #O(1)
+                C[sosed] = cena     #O(1)
+                E[sosed] = vozlisce #O(1)
     # Funkcija vrne povezave drevesa in vsoto cen med povezavemi v minimalnem vpetem drevesu
     # Vzamemo povezave od vkljucno drugega vozlisca
     F = F[1:]
-    vsota = sum([(matrika_dolzin[F[2*i]][F[2*i+1]])**2 for i in range(st_vozlisc -1) ])
+    vsota = sum([(matrika_dolzin[F[2*i]][F[2*i+1]])**2 for i in range(st_vozlisc -1) ])  #O(n)
     return [F, vsota]
